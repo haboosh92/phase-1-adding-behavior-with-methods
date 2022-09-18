c1cab16e5082d6bc4a7b179dd1c548c2e58f50df
Hiba <eng.lilil92@gmail.com>
2022-09-18 12:56:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
+
 // Your code here
+class Cat{
+constructor(name, sex){
+   // do we need a constructor here ????
+   this.name=name;
+   this.sex=sex;
+}
+speak(){
+   // do we need to pass the name property to speak method ????
+   return `${this.name} says meow!`;
+}
+}
+class Dog{
+constructor(name, sex){
+   this.name=name;
+   this.sex=sex;
+}
+speak(){
+   return `${this.name} says woof!`;
+}
+}
+class Bird{
+constructor(name, sex){
+   this.name=name;
+   this.sex=sex;
+}
+speak(){
+   if(this.sex == "male"){
+      return `It's me! ${this.name}, the parrot!`;
+   }
+   else {
+      return `${this.name} says squawk!`
+   }
+   
+}
+}
+
+const cat   = new Cat ;
+const dog   = new Dog ;
+const bird  = new Bird;
+const bird2 = new Bird;
+
+cat.speak('Sasha');
+dog.speak('Rufio');
+bird.speak('Pablo');
+bird2.speak('Mable');
